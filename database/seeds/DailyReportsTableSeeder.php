df148f6dfc582b42f13ea63e7a4ecca19f813260
Daisuke Abe <amorer2@gmail.com>
2019-05-29 18:44:22 +0900

    Seederに複数ユーザーの内容追加

--- database/seeds/DailyReportsTableSeeder.php
+++ database/seeds/DailyReportsTableSeeder.php
@@ -14,6 +14,30 @@ class DailyReportsTableSeeder extends Seeder
         DB::table('daily_reports')->truncate();
         DB::table('daily_reports')->insert([
             [
+                'user_id'    => 1,
+                'title'      =>'ユーザー１',
+                'contents'   =>'テスト１',
+                'reporting_time' => Carbon::create(2019, 3, 15, 9, 50, 22)
+            ],
+            [
+                'user_id'    => 1,
+                'title'      =>'ユーザー１-2',
+                'contents'   =>'テスト１-2',
+                'reporting_time' => Carbon::create(2019, 3, 15, 9, 50, 22)
+            ],
+            [
+                'user_id'    => 2,
+                'title'      =>'ユーザー２',
+                'contents'   =>'テスト２',
+                'reporting_time' => Carbon::create(2019, 4, 16, 9, 50, 22)
+            ],
+            [
+                'user_id'    => 3,
+                'title'      =>'ユーザー３',
+                'contents'   =>'テスト３',
+                'reporting_time' => Carbon::create(2019, 5, 17, 9, 50, 22)
+            ],
+            [
                 'user_id'    => 4,
                 'title'      =>'一覧機能実装',
                 'contents'   =>'インデックスページの編集',
@@ -29,7 +53,7 @@ class DailyReportsTableSeeder extends Seeder
                 'user_id'    => 4,
                 'title'      =>'編集機能実装',
                 'contents'   =>'editページの編集',
-                'reporting_time' => Carbon::create(2019, 1, 17, 9, 50, 22)
+                'reporting_time' => Carbon::create(2019, 2, 17, 9, 50, 22)
             ]
         ]);
     }
