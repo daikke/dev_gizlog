eff92a7cee38e247ad4792e739de3b0c8b2980c4
Daisuke Abe <amorer2@gmail.com>
2019-05-31 11:25:24 +0900

    if文の記載を三項演算子に変更。

--- resources/views/user/daily_report/create.blade.php
+++ resources/views/user/daily_report/create.blade.php
@@ -8,30 +8,18 @@
   @else
     <div class="container">
       {!! Form::open(['route' => 'daily_report.store']) !!}
-        @if ($errors->has('reporting_time'))
-          <div class="form-group form-size-small has-error">
-        @else
-          <div class="form-group form-size-small">
-        @endif
-            {!! Form::input('date', 'reporting_time', Carbon::now()->format('Y-m-d'), ['class' => 'form-control form-require-input']) !!}
-            <span class="help-block">{{ $errors->first('reporting_time') }}</span>
-          </div>
-        @if ($errors->has('title'))
-          <div class="form-group has-error">
-        @else
-          <div class="form-group">
-        @endif
-            {!! Form::input('text', 'title', null, ['class' => 'form-control', 'placeholder' => 'Title']) !!}
-            <span class="help-block">{{ $errors->first('title') }}</span>
-          </div>
-        @if ($errors->has('contents'))
-          <div class="form-group has-error">
-        @else
-          <div class="form-group">
-        @endif
-            {!! Form::textarea('contents', null, ['class' => 'form-control', 'placeholder' => 'Content']) !!}
-            <span class="help-block">{{ $errors->first('contents') }}</span>
-          </div>
+        <div class="form-group form-size-small {{ $errors->has('reporting_time') ? 'has-error' : '' }}">
+          {!! Form::input('date', 'reporting_time', Carbon::now()->format('Y-m-d'), ['class' => 'form-control form-require-input']) !!}
+          <span class="help-block">{{ $errors->first('reporting_time') }}</span>
+        </div>
+        <div class="form-group {{ $errors->has('title') ? 'has-error' : '' }}">
+          {!! Form::input('text', 'title', null, ['class' => 'form-control', 'placeholder' => 'Title']) !!}
+          <span class="help-block">{{ $errors->first('title') }}</span>
+        </div>
+        <div class="form-group {{ $errors->has('contents') ? 'has-error' : '' }}">
+          {!! Form::textarea('contents', null, ['class' => 'form-control', 'placeholder' => 'Content']) !!}
+          <span class="help-block">{{ $errors->first('contents') }}</span>
+        </div>
         {!! Form::submit('Add', ['class' => 'btn btn-success pull-right']) !!}
       {!! Form::close() !!}
     </div>
