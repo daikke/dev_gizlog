dde212c35f2177816eff2ea481e89df103b1b01f
Daisuke Abe <amorer2@gmail.com>
2019-06-10 18:17:11 +0900

    バリデーションエラーの表示を追加

--- resources/views/user/question/edit.blade.php
+++ resources/views/user/question/edit.blade.php
@@ -12,15 +12,15 @@
             <option value="{{ $objectTagCategory->id }}" {{ $objectTagCategory->id === $objectSearchedQuestion->tag_category_id ? 'selected' : '' }}>{{ $objectTagCategory->name }}</option>
           @endforeach
         </select>
-        <span class="help-block"></span>
+        <span class="help-block">{{ $errors->first('tag_category_id') }}</span>
       </div>
       <div class="form-group">
         <input class="form-control" placeholder="title" name="title" type="text" value="{{ $objectSearchedQuestion->title }}">
-        <span class="help-block"></span>
+        <span class="help-block">{{ $errors->first('title') }}</span>
       </div>
       <div class="form-group">
         <textarea class="form-control" placeholder="{{ $objectSearchedQuestion->content }}" name="content" cols="50" rows="10"></textarea>
-        <span class="help-block"></span>
+        <span class="help-block">{{ $errors->first('content') }}</span>
       </div>
       <input name="confirm" class="btn btn-success pull-right" type="submit" value="update">
     {{ Form::close() }}
